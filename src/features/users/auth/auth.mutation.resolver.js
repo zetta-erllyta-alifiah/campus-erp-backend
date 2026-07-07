ba68f22abb6e474e2e4a21e00dbbb6d4db40773d
Erllyta Rachma <erllyta.alifiah@zetta-intern.space>
2026-07-07 16:02:52 +0700

    Fixed: In code documentation

--- src/features/users/auth/auth.mutation.resolver.js
+++ src/features/users/auth/auth.mutation.resolver.js
@@ -1,5 +1,7 @@
-// *************** IMPORT MODULE ***************
+// *************** IMPORT HELPER ***************
 const { LoginHelper } = require('./auth.helper');
+
+// *************** IMPORT UTILITIES ***************
 const { NormalizeGqlError } = require('../../../core/errors');
 
 // *************** MUTATION ***************
